1d251845fe4f05fed75779172cdae5a0f8ce8aba
Sebastian Villalobos <dsvillacorta@gmail.com>
2023-11-09 16:04:14 -0600

    DS Spendify Version 1.0.1

---
+++ JavaScript/Add-Expense.js
@@ -0,0 +1,75 @@
+//JavaScript
+
+const addExpenseForm = document.getElementById("addExpenseForm");
+
+addExpenseForm.addEventListener("submit", addExpense);
+
+let expenses;
+
+if (localStorage.getItem("expenses") === null) {
+  expenses = [];
+} else {
+  expenses = JSON.parse(localStorage.getItem("expenses"));
+}
+
+class Expense {
+  constructor(
+    expenseName,
+    expenseDate,
+    expenseCategory,
+    expenseAccount,
+    expenseAmount
+  ) {
+    this.expenseName = expenseName;
+    this.expenseDate = expenseDate;
+    this.expenseCategory = expenseCategory;
+    this.expenseAccount = expenseAccount;
+    this.expenseAmount = expenseAmount;
+  }
+}
+
+function addExpense() {
+  event.preventDefault();
+  const expenseName = document.getElementById("expenseName").value.trim();
+  const expenseDate = document.getElementById("expenseDate").value.trim();
+  const expenseCategory = document
+    .getElementById("expenseCategory")
+    .value.trim();
+  const expenseAccount = document.getElementById("expenseAccount").value.trim();
+  const expenseAmount = document.getElementById("expenseAmount").value.trim();
+
+  const newExpense = new Expense(
+    expenseName,
+    expenseDate,
+    expenseCategory,
+    expenseAccount,
+    expenseAmount
+  );
+
+  expenses.push(newExpense);
+
+  localStorage.setItem("expenses", JSON.stringify(expenses));
+
+  addExpenseForm.reset();
+
+  let alertElement = document.createElement("div");
+  alertElement.classList.add(
+    "alert",
+    "alert-primary",
+    "alert-dismissible",
+    "fade",
+    "show",
+    "mt-3"
+  );
+  alertElement.innerHTML = `
+  <strong>Success!</strong> DS Spendify has added your Expense.
+  <button
+    type="button"
+    class="btn-close"
+    data-bs-dismiss="alert"
+    aria-label="Close"
+  ></button>
+  `;
+
+  addExpenseForm.appendChild(alertElement);
+}
---
+++ JavaScript/Add-Income.js
@@ -0,0 +1,64 @@
+//JavaScript
+
+const addIncomeForm = document.getElementById("addIncomeForm");
+
+addIncomeForm.addEventListener("submit", addIncome);
+
+let incomes;
+
+if (localStorage.getItem("incomes") === null) {
+  incomes = [];
+} else {
+  incomes = JSON.parse(localStorage.getItem("incomes"));
+}
+
+class Income {
+  constructor(incomeName, incomeDate, incomeAccount, incomeAmount) {
+    this.incomeName = incomeName;
+    this.incomeDate = incomeDate;
+    this.incomeAccount = incomeAccount;
+    this.incomeAmount = incomeAmount;
+  }
+}
+
+function addIncome() {
+  event.preventDefault();
+  const incomeName = document.getElementById("incomeName").value.trim();
+  const incomeDate = document.getElementById("incomeDate").value.trim();
+  const incomeAccount = document.getElementById("incomeAccount").value.trim();
+  const incomeAmount = document.getElementById("incomeAmount").value.trim();
+
+  const newIncome = new Income(
+    incomeName,
+    incomeDate,
+    incomeAccount,
+    incomeAmount
+  );
+
+  incomes.push(newIncome);
+
+  localStorage.setItem("incomes", JSON.stringify(incomes));
+
+  addIncomeForm.reset();
+
+  let alertElement = document.createElement("div");
+  alertElement.classList.add(
+    "alert",
+    "alert-success",
+    "alert-dismissible",
+    "fade",
+    "show",
+    "mt-3"
+  );
+  alertElement.innerHTML = `
+  <strong>Success!</strong> DS Spendify has added your Income.
+  <button
+    type="button"
+    class="btn-close"
+    data-bs-dismiss="alert"
+    aria-label="Close"
+  ></button>
+  `;
+
+  addIncomeForm.appendChild(alertElement);
+}
